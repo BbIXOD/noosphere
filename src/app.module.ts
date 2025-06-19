b63c15a732fc9064dd3dfeec59b7e99c01b24562
Maksym Diachok <maksdyachok2005@gmail.com>
2025-06-19 17:14:03 +0300

    Project module structure with dummy auth endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { KnowledgeModule } from './knowledge/knowledge.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, KnowledgeModule],
   controllers: [AppController],
   providers: [AppService],
 })
